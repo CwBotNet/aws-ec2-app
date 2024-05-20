87b8ef8c289f656dc0860ff14196d0ce7f591c16
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-05-20 22:41:07 +0530

    bug fix

--- index.ts
+++ index.ts
@@ -2,8 +2,14 @@ import express from "express";
 
 const app = express();
 
+const PORT = 3000;
+
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "Hello aws ec2",
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port : ${PORT}`);
 });
